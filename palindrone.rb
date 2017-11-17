1e39936498d5e4d4d5fb0964cc60ffd0fd73ff33
Isabel Juliao-Martin <isabel.youthboard@futureindesign.com>
2017-11-17 10:14:10 -0700

    updating palindrome

--- palindrone.rb
+++ palindrone.rb
@@ -1,10 +1,21 @@
 	#
+	require 'colorize'
 
-	def palindrone
-		puts "Palindrone checker"
-		puts "Type in a word to see if its the same backwards as it is forwards!"
-		palin = gets.string 
-		check 
-	end
-
-	def check
+	def palindrome
+		puts "Palindrome checker".cyan
+		puts "Type in a word to see if its the same backwards as it is forwards!".yellow
+		puts "(type quit to exit)".magenta
+		palin = gets.strip.downcase
+		case palin
+			when 'quit', 'exit'
+				exit
+			when palin.reverse 
+				puts "It is a palindrome!".red
+			else
+				puts "Not a palindrome, my dude".red
+			end
+			palindrome
+		end
+		palindrome
+	
+				
