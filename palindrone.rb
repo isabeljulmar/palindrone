808d3f1463b11c86ee1dfa7e22527a86df3a4f02
Isabel Juliao-Martin <isabel.youthboard@futureindesign.com>
2017-11-17 09:36:43 -0700

    adding palindrone

---
+++ palindrone.rb
@@ -0,0 +1,10 @@
+	#
+
+	def palindrone
+		puts "Palindrone checker"
+		puts "Type in a word to see if its the same backwards as it is forwards!"
+		palin = gets.string 
+		check 
+	end
+
+	def check
